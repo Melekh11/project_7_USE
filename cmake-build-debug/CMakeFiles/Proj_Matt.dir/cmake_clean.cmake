bc21bc30305625e49af64ba6623f63422a60b103
Матвей <cktkutxd@gmail.com>
2022-03-08 18:18:27 +0300

    1st

---
+++ cmake-build-debug/CMakeFiles/Proj_Matt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proj_Matt.dir/main.cpp.o"
+  "Proj_Matt"
+  "Proj_Matt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proj_Matt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
